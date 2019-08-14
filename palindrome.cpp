6b2e04978fc9be9ace3334ae0f822b5b77b85637
Lawrence Warren <lawrencewarren2@gmail.com>
2019-08-14 13:10:54 +0100

    Framework setup - now add string reversing and checking!

--- palindrome.cpp
+++ palindrome.cpp
@@ -12,10 +12,27 @@ bool sanitise(int num, char word[])
     else { return true; }
 }
 
+bool check(char word[])
+{
+    char* backwards;
+
+    //Some code
+
+    return false;
+}
+
+
 int main(int argc, char* argv[])
 {
     if(sanitise(argc, argv[1])==false)
     {
         std::cout << "Please enter a valid argument.";
+        return 1;
     }
+
+    if (check(argv[1])==true) { std::cout << "\"" << argv[1] << "\" is a palindrome"; }
+
+    else { std::cout << "\""<< argv[1] << "\" is not a palindrome."; }
+    
+    return 0;
 }
