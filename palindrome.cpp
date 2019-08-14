bcd1717e741a03b3f0fa6549355eb64289c14df5
Lawrence Warren <lawrencewarren2@gmail.com>
2019-08-14 12:59:00 +0100

    Sanitised command line arguments

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,1 +1,21 @@
 //A program which checks to see if a word is a palindrome.
+#include <iostream>
+#include <cstring>
+
+bool sanitise(int num, char word[])
+{
+    if (num==1 || strlen(word)<=1)
+    {
+        return false;
+    }
+
+    else { return true; }
+}
+
+int main(int argc, char* argv[])
+{
+    if(sanitise(argc, argv[1])==false)
+    {
+        std::cout << "Please enter a valid argument.";
+    }
+}
