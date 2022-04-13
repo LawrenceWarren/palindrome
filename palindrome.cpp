1c662936d269937b1430a0ace2060b645a8227dd
Lawrence Warren <lawrencewarren2@gmail.com>
2022-04-13 11:03:36 +0100

    Fix comments, additional formatting

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,8 +1,8 @@
-// A program which checks to see if a word is a palindrome.
+// A program which checks to see if a word is a palindrome
 #include <cstring>
 #include <iostream>
 
-// Checks to see if the given argument is a valid word.
+// Checks to see if the given argument is a valid word
 bool sanitise(int num, char* word) {
   if (num == 1 || strlen(word) <= 1) {
     return false;
@@ -18,13 +18,12 @@ bool sanitise(int num, char* word) {
   return true;
 }
 
-char* caser(char* word)  // Converts all of the letters to lower case.
-{
+// Converts all of the letters to lower case
+char* caser(char* word) {
   char* temp = word;
 
-  for (int i = strlen(word) - 1; i >= 0;
-       i--)  // Loops through every char in the array.
-  {
+  // Loops through every char in the array
+  for (int i = strlen(word) - 1; i >= 0; i--) {
     if (temp[i] < 97) {
       temp[i] += 32;
     }  // Converts each char to lower case if it is upper case.
@@ -33,38 +32,33 @@ char* caser(char* word)  // Converts all of the letters to lower case.
   return temp;
 }
 
-bool check(char* word)  // Actually checks if the word is a palindrome.
-{
+// Actually checks if the word is a palindrome
+bool check(char* word) {
   int length = strlen(word) - 1;
 
-  for (int i = 0; length > i;
-       length--, i++)  // One counter starts at the beginning of the word, one
-                       // starts at the end, and they work inwards. The counters
-                       // essentially represent the mirrored side of the word,
-                       // and they are compared.
-  {
+  // One counter starts at the beginning of the word, one starts at the end, and
+  // they work inwards. The counters essentially represent the mirrored side of
+  // the word, and they are compared.
+  for (int i = 0; length > i; length--, i++) {
+    // if counter 1 is different to counter 2, the word is not a palindrome
     if (word[i] != word[length]) {
       return false;
-    }  // if the value of counter 1 is different to counter 2, the word is not a
-       // palindrome.
+    }
   }
 
   return true;
 }
 
-int main(int argc, char* argv[])  // Runs the above functions.
-{
-  if (sanitise(argc, argv[1]) == false)  // If the word is not sanitised, fail.
-  {
+int main(int argc, char* argv[]) {
+  // If the word is not sanitised, fail
+  if (sanitise(argc, argv[1]) == false) {
     std::cout << "Please enter a valid word.";
     return 1;
   }
 
   if (check(caser(argv[1])) == true) {
     std::cout << "\"" << caser(argv[1]) << "\" is a palindrome";
-  }
-
-  else {
+  } else {
     std::cout << "\"" << caser(argv[1]) << "\" is not a palindrome.";
   }
 
